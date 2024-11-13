3dae98ef97baafb63462eb39e66a4c10ccdfccc8
Gustajb <gustavobatistagb09@gmail.com>
2024-11-13 19:44:18 +0000

    salvar

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.database import Session
+import os
+
+session = Session()
+repository = UsuarioRepository(session)
+service = UsuarioService(repository)
+
+# Limpa o terminal.
+os.system("cls || clear")
+
+# Solicitando dados para o usuário.
+print("\nCadastrando usuário: ")
+print("\nDigite seu nome: ")
+print("\nDigite seu e-mail: ")
+print("\nDigite sua senha: ")
+
+service.criar_usuario(nome, email, senha)
+
+# Exibindo todos os registros na tabela "usuarios" do banco de dados.
+print("\n===LIstando usuários cadastrados===")
+lista_usuarios = service.listar_todos_usuarios()
+for usuario in lista_usuarios:
+    print(f"Nome: {usuario.nome} \nE-mail: {usuario.email}")
---
+++ app/services/usuario_service.py
@@ -0,0 +1,26 @@
+from models.usuario_model import Usuario
+from repositories.usuario_repository import UsuarioRepository
+
+class UsuarioService:
+    def __init__(self, UsuarioRepository):
+        self.repository = repository 
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario(nome=nome, email=email, senha=senha)
+
+            usuario_cadastrado = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if usuario_cadastrado:
+                print("Usúario já cadastrado!")
+                return
+            
+            self.repository.salvar_usuario(usuario)
+            print("Usuário cadastrado com sucesso!")
+        except TypeError as erro:
+            print(f"Erro ao salvar usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")
+
+    def listar_todos_usuarios(self):
+        return self.repository.listar_usuarios()
